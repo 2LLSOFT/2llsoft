36fb629d411f9e1036f71b6f229a068673b5285c
2LLSOFT <ozguraracii13@gmail.com>
2026-05-19 15:13:37 +0200

    new fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,17 @@
 export default function Home() {
   return (
-    <main style={{
-      minHeight: "100vh",
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-      background: "#000",
-      color: "#fff",
-      fontFamily: "Arial"
-    }}>
-      <div style={{ textAlign: "center" }}>
-        <h1 style={{ fontSize: "60px" }}>2LLSOFT</h1>
+    <main
+      style={{
+        minHeight: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "black",
+        color: "white",
+      }}
+    >
+      <div>
+        <h1 style={{ fontSize: "64px" }}>2LLSOFT</h1>
         <p>Modern Software Solutions</p>
       </div>
     </main>
